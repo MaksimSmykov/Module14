9ea70776f23b71aa7fc75d8ba9c727402978baf8
MaksimSmykov <smykovmaksim23@yandex.ru>
2025-01-19 13:01:10 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -12,9 +12,32 @@ def initiate_db():
     price INT NOT NULL
     ); 
     ''')
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    ); 
+    ''')
     connection.commit()
 
+def add_user(username, email, age):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (username, email, age, 1000))
+    connection.commit()
+
+def is_included(user_name):
+    check_user = cursor.execute('SELECT * FROM Users WHERE username = ?', (user_name,))
+    if check_user.fetchone() is not None:
+        connection.commit()
+        return True
+    connection.commit()
+    return False
+
 def get_all_products():
+    connection.commit()
     return cursor.execute('SELECT * FROM Products')
 
 # initiate_db()
